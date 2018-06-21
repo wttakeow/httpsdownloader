b8da36f0ba6b95fb3ac9589bd6bdba2650013926
Takeow <Wisoodthipong.Takeow@thomsonreuters.com>
2018-06-21 16:50:46 +0700

    first commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            FileStream m_outputStream;
+            string tempFile = "nicefile.zip";
+            FileInfo fileInfo = new FileInfo(tempFile);
+            if (!Directory.Exists(fileInfo.DirectoryName))
+            {
+                Directory.CreateDirectory(fileInfo.DirectoryName);
+            }
+            Stream downloadStream = null;
+            m_outputStream = new FileStream(tempFile, FileMode.Create, FileAccess.Write);
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+            string url = "https://github.com/electron/electron-api-demos/archive/master.zip";
+            WebRequest request = WebRequest.Create(url);
+            HttpWebRequest httpRequest = (HttpWebRequest)request;
+            HttpWebResponse response = (HttpWebResponse)httpRequest.GetResponse();
+            long fileSize = response.ContentLength;
+            m_outputStream.SetLength(Math.Max(fileSize, 0));
+            downloadStream = response.GetResponseStream();
+            long readSize = 0;
+            int buffSize = 8192;
+            byte[] buffer = new byte[buffSize];
+            do
+            {
+                readSize = downloadStream.Read(buffer, 0, buffSize);
+
+            
+                m_outputStream.Write(buffer, 0, (int)readSize);
+
+
+            } while (readSize > 0);
+
+        }
+
+        
+
+        //request.Timeout = 3000;
+
+        //request.Proxy = CreateProxy(url, proxyServer, proxyUsername, proxyPassword);
+        //
+       
+
+    }
+
+
+
+
+
+
+
+}
+
+
---
+++ ConsoleApplication1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("TEST")]
+[assembly: AssemblyProduct("ConsoleApplication1")]
+[assembly: AssemblyCopyright("Copyright © TEST 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5ec58166-a731-464e-af26-66f14b47e382")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
